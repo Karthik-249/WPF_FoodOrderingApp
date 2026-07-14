70f7f76b3627c96776d4238ea65c57d549f47513
Karthik Kannan <karthikk249@gmail.com>
2026-07-14 14:53:04 +0530

    Feature | Present the menu item cards with suitable visual icons for better UI/UX
    1. Added icons for item type (veg/non-veg/eggs) and also for rating levels (excellent/good/decent/poor).
    2. Added converters to convert view model properties to icon paths for better UI representation.
    
    Signed-off-by: Karthik Kannan <karthikk249@gmail.com>

---
+++ WPF_FoodOrderingApp/Converters/ItemTypeToImagePathConverter.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using WPF_FoodOrderingApp.Reusables.Enums;
+
+namespace WPF_FoodOrderingApp.Converters;
+
+public class ItemTypeToImagePathConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        ItemType type = (ItemType)value;
+
+        return type switch
+        {
+            ItemType.Veg => "../Resources/Icons/ItemTypes/VegIcon.png",
+            ItemType.NonVeg => "../Resources/Icons/ItemTypes/NonVegIcon.png",
+            ItemType.Egg => "../Resources/Icons/ItemTypes/ContainsEggIcon.png",
+            _ => "../Resources/Icons/ItemTypes/NonVegIcon.png"
+        };
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ WPF_FoodOrderingApp/Converters/RatingValueToImagePathConverter.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace WPF_FoodOrderingApp.Converters;
+
+public class RatingValueToImagePathConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        double.TryParse(value.ToString(), out double ratingValue);
+
+        if (ratingValue >= 4.5)
+        {
+            return "../Resources/Icons/Ratings/ExcellentRatingIcon.png";
+        }
+        else if (ratingValue >= 4)
+        {
+            return "../Resources/Icons/Ratings/GoodRatingIcon.png";
+        }
+        else if (ratingValue >= 3.5)
+        {
+            return "../Resources/Icons/Ratings/DecentRatingIcon.png";
+        }
+        else
+        {
+            return "../Resources/Icons/Ratings/PoorRatingIcon.png";
+        }
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
